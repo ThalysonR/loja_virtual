e573acc8006be8e2736c681ea1d2222ccb1ed81a
Thalyson <Thalysonr.castro@hotmail.com>
2019-06-27 23:56:04 -0400

    Prettier / Ajuste paginação front

--- front/src/app/modules/produto/components/catalogo/catalogo.component.ts
+++ front/src/app/modules/produto/components/catalogo/catalogo.component.ts
@@ -1,41 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import { ProdutoControllerService, Produto } from '../../../../../swagger';
 import { ActivatedRoute } from '@angular/router';
-import { map, flatMap } from 'rxjs/operators';
+import { flatMap, map } from 'rxjs/operators';
+import { Produto, ProdutoControllerService } from '../../../../../swagger';
 
 @Component({
-    selector: 'produto-catalogo',
-    templateUrl: './catalogo.component.html',
-    styleUrls: ['./catalogo.component.scss']
+  selector: 'produto-catalogo',
+  templateUrl: './catalogo.component.html',
+  styleUrls: ['./catalogo.component.scss'],
 })
-
 export class CatalogoComponent implements OnInit {
-    constructor(private _produtoController: ProdutoControllerService,
-        private route: ActivatedRoute) {}
+  constructor(
+    private _produtoController: ProdutoControllerService,
+    private route: ActivatedRoute,
+  ) {}
 
-    public produtos: Array<Produto> = [];
-    public paginaAtual: number = 0;
-    public paginas: Array<number>;
-    
-    ngOnInit() {
-        const size = '12';
-        this.route.queryParams
-        .pipe(
-            map(params => {
-                if (params.page == null) {
-                    return {page: '0'}
-                } else {
-                    return params;
-                }
-            }),
-            flatMap(params => this._produtoController.getProdutosUsingGET(params.page, size))
-        )
-        .subscribe(prodPage => {
-            console.log(prodPage);
-            this.produtos = prodPage.content;
-            this.paginaAtual = prodPage.number;
-            // @ts-ignore
-            this.paginas = Array(prodPage.totalPages).fill().map((x,i)=>i);
-        });
-    }
+  public produtos: Array<Produto> = [];
+  public paginaAtual: number = 0;
+  public paginas: Array<number>;
+  public totalPaginas: number = 0;
+
+  ngOnInit() {
+    const size = '12';
+    this.route.queryParams
+      .pipe(
+        map(params => {
+          if (params.page == null) {
+            return { page: '0' };
+          } else {
+            return params;
+          }
+        }),
+        flatMap(params => this._produtoController.getProdutosUsingGET(params.page, size)),
+      )
+      .subscribe(prodPage => {
+        this.produtos = prodPage.content;
+        this.paginaAtual = prodPage.number;
+        // @ts-ignore
+        this.paginas = Array(prodPage.totalPages)
+          .fill()
+          // @ts-ignore
+          .map((x, i) => i);
+        this.totalPaginas = prodPage.totalPages;
+      });
+  }
 }
